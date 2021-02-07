f62367a2b36b5ab703e1ae2c0e1e6e8a47061e90
l.adamus <l.adamus@sportradar.com>
2021-02-07 16:48:12 +0100

    frontend is able to display sites fetched from backend (without compound properties)

--- archeoscribe-react.js
+++ archeoscribe-react.js
@@ -1,36 +1,85 @@
+class Sites extends React.Component {  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      archeoSites: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/archeoscribe/restapi/v0/sites/")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          archeoSites: result
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, archeoSites } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+        <p>Sites</p>
+        <ul>
+        {archeoSites.map((archeoSite, index) => {
+          return <li key={index}> <Site archeoSite={archeoSite} /></li>
+        })}
+        </ul>
+        </div>
+        );
+    }
+  }
+
+
+}
+
 class Site extends React.Component {   
-        constructor(props) {
-          super(props);
-          this.archeoproperties = [
-            {"name": "Name", "value": "some site"},
-            {"name": "Location", "value": "neverland"}
-          ];
-        }
-        
-        render() {
-           return (
-            <div>
-              <p>Site</p>
-              <ul>
-                {this.archeoproperties.map((archeoproperty, index) => {
-                  return <li key={index}>{archeoproperty.name} : {archeoproperty.value}</li>
-                })}
-            </ul>
-           </div>
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+   return (
+    <div>
+    <p>Site {this.props.archeoSite.id}</p>
+    <ul>
+    {this.props.archeoSite.properties.map((archeoProperty, index) => {
+      return <li key={index}>{archeoProperty.propertyName} : {archeoProperty.propertyValue}</li>
+    })}
+    </ul>
+    </div>
     );
   }
 }
 
 ReactDOM.render(
-<div>
+  <div>
   <h1>Hello ArcheoScribe React!</h1>
-  <Site /> 
- </div>,
- document.getElementById('root')
-);
+  <Sites /> 
+  </div>,
+  document.getElementById('root')
+  );
 
-    
-    
-    
-    
+
+
+
+
     
